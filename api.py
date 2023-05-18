d5a583c9900a6f39781008cd51f83037967b0358
CAIO PALERMO LEMOS <22102544@CFP127ALUNO.LOCAL>
2023-05-18 16:12:15 -0300

    alo

--- api.py
+++ api.py
@@ -1,8 +1,8 @@
 import requests
 from urllib3.exceptions import InsecureRequestWarning
-from flask import Flask, request
+from flask import Flask, request, jsonify
 
-print('Servidor rodando na porta 5000')
+print('------- SERVER LISTENING ON PORT 5000 -------')
 
 app = Flask(__name__)
 
@@ -14,9 +14,9 @@ password = 'senai127'
 
 @app.route('/api/exemplo', methods=['POST'])
 def exemplo():
-    usernameData = request.form.get('username')  # Obtém o valor do parâmetro 'variavel' enviado na solicitação POST
-    passwordData = request.form.get('password')  # Obtém o valor do parâmetro 'variavel' enviado na solicitação POST
-    expiresData = request.form.get('expires')  # Obtém o valor do parâmetro 'variavel' enviado na solicitação POST
+    usernameData = request.form.get('username') 
+    passwordData = request.form.get('password') 
+    expiresData = request.form.get('expires') 
 
     # Dados a serem enviados no POST no formato JSON
     data = {
@@ -35,10 +35,12 @@ def exemplo():
 
     if response.status_code == 200:
         # Requisição POST bem-sucedida
-        print('POST request successful')
+        #print('POST request successful')
+        return jsonify({'message': f'POST request successful! {response.status_code}'})
     else:
         # Tratar erros na requisição POST
-        print('Error making POST request:', response.status_code)
+        #print('Error making POST request:', response.status_code)
+        return jsonify({'message': f'Error making POST request: {response.status_code}'})
 
 # Executar o servidor Flask
 if __name__ == '__main__':
